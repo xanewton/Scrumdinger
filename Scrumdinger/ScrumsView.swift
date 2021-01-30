f4b56428efda9698fbea4352937561619c8defc6
Angel Newton <xanewton@gmail.com>
2021-01-30 16:06:50 -0500

    Display a List of Daily Scrums

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by newtan on 2021-01-30.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
