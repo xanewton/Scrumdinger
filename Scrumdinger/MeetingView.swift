204da50971cde82bd7909be9075ccb5e60dc6e34
Angel Newton <xanewton@gmail.com>
2021-02-02 18:10:09 -0500

    Create an Overlay View

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,47 +8,54 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    // Image from SF Symbols 2 https://developer.apple.com/sf-symbols/
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.color)
+            VStack {
+                ProgressView(value: 5, total: 15)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Seconds Elapsed")
+                            .font(.caption)
+                        // Image from SF Symbols 2 https://developer.apple.com/sf-symbols/
+                        Label("300", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("Seconds Remaining")
+                            .font(.caption)
+                        Label("600", systemImage: "hourglass.tophalf.fill")
+                    }
                 }
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.tophalf.fill")
+                /*
+                 Accessibility is built into SwiftUI, so you get accessibility support with little additional work. For example, string content in Text views is automatically accessible to device features like VoiceOver
+                 https://www.apple.com/accessibility/vision/
+                 */
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel(Text("Time remaining"))
+                .accessibilityValue(Text("10 minutes"))
+                Circle()
+                    .strokeBorder(lineWidth: 24, antialiased: true)
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel(Text("Next speaker"))
                 }
-            }
-            /*
-             Accessibility is built into SwiftUI, so you get accessibility support with little additional work. For example, string content in Text views is automatically accessible to device features like VoiceOver
-             https://www.apple.com/accessibility/vision/
-             */
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel(Text("Time remaining"))
-            .accessibilityValue(Text("10 minutes"))
-            Circle()
-                .strokeBorder(lineWidth: 24, antialiased: true)
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
-                }
-                .accessibilityLabel(Text("Next speaker"))
             }
         }
         .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.data[0]))
     }
 }
