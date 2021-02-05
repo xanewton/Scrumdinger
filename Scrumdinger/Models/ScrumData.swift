00a4381b4cce4dd829f970802a208507d8e12631
Angel Newton <xanewton@gmail.com>
2021-02-04 19:26:04 -0500

    Add a Method to Load Data

--- Scrumdinger/Models/ScrumData.swift
+++ Scrumdinger/Models/ScrumData.swift
@@ -24,4 +24,27 @@ class ScrumData: ObservableObject {
     
     // An ObservableObject includes an objectWillChange publisher that emits when one of its @Published properties is about to change. Any view observing an instance of ScrumData will re-render when the scrums value changes.
     @Published var scrums: [DailyScrum] = []
+    
+    func load() {
+        // Dispatch queues are first in, first out (FIFO) queues to which your application can submit tasks. Background tasks have the lowest priority of all tasks.
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            // Load the contents of scrums.data into a local constant named data.
+            guard let data = try? Data(contentsOf: Self.fileURL) else {
+                #if DEBUG
+                // Important. Always perform UI updates on the main queue.
+                DispatchQueue.main.async {
+                    self?.scrums = DailyScrum.data
+                }
+                #endif
+                return
+            }
+            guard let dailyScrums = try? JSONDecoder().decode([DailyScrum].self, from: data) else {
+                fatalError("Can't decode saved scrum data.")
+            }
+            // On the main queue, set scrums equal to dailyScrums.
+            DispatchQueue.main.async {
+                self?.scrums = dailyScrums
+            }
+        }
+    }
 }
